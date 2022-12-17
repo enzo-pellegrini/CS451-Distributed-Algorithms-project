40f59dbdf0be1a51726c34d3c2c4c4d0718e26bf
Vincenzo Pellegrini <dev@enzopellegrini.me>
2022-12-17 18:24:25 +0100

    Current submission, could benefit from: - memory optimization on nacks - object pool (or reuing the same object in some parts of the code) - looking at consensusManager more, for concurrency issues (deadlock possible maybe)

--- template_java/src/main/java/cs451/PerfectLinks/DirectedSender.java
+++ template_java/src/main/java/cs451/PerfectLinks/DirectedSender.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -19,11 +20,13 @@ public class DirectedSender<T> {
     static private final int MAX_IN_FLIGHT = 10;
 
     private final PerfectLink<T> parent;
-    private final int dstId;
+    final int dstId;
     private final Queue<T> queue = new LinkedList<>();
 
     private int seqNum = 0;
-    private Map<Integer, List<T>> sent = new HashMap<>();
+    private Map<Integer, List<T>> inFlight = new HashMap<>();
+
+    int resendPause = 200;
 
     public DirectedSender(PerfectLink<T> parent, int dstId) {
         this.parent = parent;
@@ -38,27 +41,44 @@ public class DirectedSender<T> {
 
     public int send(DatagramSocket sock) {
         // resent all packages in sent,
-        // then send packages from messages in queue, by putting at most 8 messages in each package, 
+        // then send packages from messages in queue, by putting at most 8 messages in
+        // each package,
         // and sending at most MAX_IN_FLIGHT packages
-        // return the number of packages sent from the queue (not resent), or 10 if the queue is empty
+        // return the number of packages sent from the queue (not resent), or 10 if the
+        // queue is empty
         synchronized (this) {
             int newPackagesSent = 0;
-            while (sent.size() < MAX_IN_FLIGHT && !queue.isEmpty()) {
+            while (inFlight.size() < MAX_IN_FLIGHT && !queue.isEmpty()) {
                 List<T> messages = new ArrayList<>();
                 while (!queue.isEmpty() && messages.size() < 8) {
-                    messages.add(queue.poll());
+                    var m = queue.poll();
+                    // if (parent.isCanceled.apply(m, dstId)) {
+                    // System.out.println("Dropping message " + m);
+                    // continue;
+                    // }
+                    messages.add(m);
                 }
                 int n = this.seqNum++;
-                // parent.sendPackage(sock, new Package<>(messages, seqNum), dstId);
-                sent.put(n, messages);
+                inFlight.put(n, messages);
                 newPackagesSent++;
             }
 
+            boolean hadError = false;
             // iterate over entries in sent
-            for (Map.Entry<Integer, List<T>> entry : sent.entrySet()) {
-                // parent.sendPackage(sock, new Package<>(entry.getValue().messages, entry.getValue().seqNum), dstId);
-                sendPackage(sock, entry.getKey(), entry.getValue());
+            for (Map.Entry<Integer, List<T>> entry : inFlight.entrySet()) {
+                // parent.sendPackage(sock, new Package<>(entry.getValue().messages,
+                // entry.getValue().seqNum), dstId);
+                if (!sendPackage(sock, entry.getKey(), entry.getValue())) {
+                    hadError = true;
+                    break;
+                }
             }
+            if (hadError) {
+                return 0;
+            }
+
+            // System.out.println("[to " + dstId + "] inFlight.size() = " + inFlight.size()
+            // + ", queue.size() = " + queue.size());
 
             // if the queue is empty, we want to go as fast as possible, so we return 10
             return queue.isEmpty() ? MAX_IN_FLIGHT : newPackagesSent;
@@ -67,20 +87,27 @@ public class DirectedSender<T> {
 
     public void handleAck(AckPacket ack) {
         synchronized (this) {
-            sent.remove(ack.n);
+            inFlight.remove(ack.n);
+            // System.out.println("Received ack for " + ack.n + " from " + ack.receiver_id);
         }
     }
 
-    private void sendPackage(DatagramSocket sock, int n, List<T> messages) {
+    private boolean sendPackage(DatagramSocket sock, int n, List<T> messages) {
+        // TODO: reuse the same datagram packet for all messages
         DataPacket<T> p = new DataPacket<>(n, parent.myId, messages);
         byte[] buf = parent.serializer.serialize(p);
         Host dst = parent.hosts.get(dstId - 1);
         try {
-            // TODO: object pool for DatagramPacket
             DatagramPacket dp = new DatagramPacket(buf, buf.length, InetAddress.getByName(dst.getIp()), dst.getPort());
             sock.send(dp);
+        } catch (SocketException e) {
+            e.printStackTrace();
+            return false;
         } catch (IOException e) {
             e.printStackTrace();
+            return false;
         }
+
+        return true;
     }
 }
--- template_java/src/main/java/cs451/PerfectLinks/PerfectLink.java
+++ template_java/src/main/java/cs451/PerfectLinks/PerfectLink.java
@@ -10,16 +10,12 @@ import java.net.InetAddress;
 import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.concurrent.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class PerfectLink<T> {
-    private static final int SENDER_COUNT = 1;
     /**
      * Multiplying factor of the exponential backoff algorithm
      * NO BULLI!
@@ -27,15 +23,15 @@ public class PerfectLink<T> {
     private static final double BACKOFF_BASE_UP = 2;
     private static final double BACKOFF_BASE_DOWN = 1.5;
     /**
-     * If the average of the sentCount of the values in the resend buffer is lower
-     * than this, decrement the resendPause
+     * If the directedSender sent less than this number of new packages,
+     * increase the delay
      */
-    private static final double BACKOFF_DECREASE_UPPERBOUND = 1.1;
+    private static final double BACKOFF_INCREASE_UPPERBOUND = 3;
     /**
-     * If the average of the sentCount of the values in the resend buffer is higher
-     * than this, increment the resendPause
+     * If the directedSender sent more than this number of new packages,
+     * decrease the delay
      */
-    private static final double BACKOFF_INCREASE_LOWERBOUND = 1.5;
+    private static final double BACKOFF_DECREASE_LOWERBOUND = 7;
     /**
      * Minimum value the resend pause can assume
      */
@@ -43,39 +39,27 @@ public class PerfectLink<T> {
     /**
      * Maximum value the resend pause can assume
      */
-    private static final int MAX_RESENDPAUSE = 1000;
+    private static final int MAX_RESENDPAUSE = 2000;
     /**
      * Maximum number of packages to be sent by <code>resenderRoutine</code> before
      * sleeping
      */
-    private static final int MAX_SEND_AT_ONCE = 200;
-    private static final int HISTORY_SIZE = 20;
 
     final int myId;
     private final int port;
     final List<Host> hosts;
 
-    private final Lock sendBufferLock = new ReentrantLock();
-    private final List<List<T>> sendBuffers;
-
-    /**
-     * time to wait before resending the packets waiting to be resent
-     * follows exponential backoff
-     */
-    private int currentResendPause = 50;
-
-    private final AtomicInteger packetCount = new AtomicInteger(0);
-    private final BlockingDeque<NetworkTypes.Sendable> senderQueue = new LinkedBlockingDeque<>();
-    private final ConcurrentLinkedQueue<Sendable> resendWaitingQueue = new ConcurrentLinkedQueue<>();
-    private final Set<Integer> confirmed = ConcurrentHashMap.newKeySet();
     private final Set<ReceivedPacket> deliveredSet = Collections.synchronizedSet(new HashSet<>());
     private final Consumer<ReceivedMessage<T>> deliver;
-    private final BiFunction<T, Integer, Boolean> isCanceled;
+    final BiFunction<T, Integer, Boolean> isCanceled;
     final MSerializer<T> serializer;
 
-    private final Thread[] senderThreads = new Thread[SENDER_COUNT];
     private final Thread receiverThread;
-    private final Thread resendTimer;
+
+    private final List<DirectedSender<T>> directedSenders;
+
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final DatagramSocket sendingSocket;
 
     public PerfectLink(int myId, int port, List<Host> hosts, Consumer<ReceivedMessage<T>> deliver,
             BiFunction<T, Integer, Boolean> isCanceled,
@@ -87,34 +71,56 @@ public class PerfectLink<T> {
         this.deliver = deliver;
         this.isCanceled = isCanceled;
 
-        List<List<T>> tmp = new ArrayList<>();
+        try {
+            sendingSocket = new DatagramSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        // Initialize directedSenders
+        List<DirectedSender<T>> tmp = new ArrayList<>();
         for (int i = 0; i < hosts.size(); i++) {
-            tmp.add(new ArrayList<>(8));
+            tmp.add(new DirectedSender<>(this, i + 1));
         }
-        this.sendBuffers = Collections.unmodifiableList(tmp);
+        directedSenders = Collections.unmodifiableList(tmp);
 
-        // Start sender workers
-        for (int i = 0; i < SENDER_COUNT; i++) {
-            senderThreads[i] = new Sender(i);
-        }
-
-        // Start receiver worker
+        // Create receiver thread
         receiverThread = new Receiver();
-
-        resendTimer = new Thread(this::resenderRoutine);
     }
 
     /**
      * Start all needed threads
      */
     public void startThreads() {
-        for (int i = 0; i < SENDER_COUNT; i++) {
-            senderThreads[i].start();
+        receiverThread.start();
 
-            receiverThread.start();
-
-            resendTimer.start();
+        for (DirectedSender<T> ds : directedSenders) {
+            if (ds.dstId == myId)
+                continue;
+            executor.submit(() -> sendRoutine(ds.dstId));
         }
+    }
+
+    private void sendRoutine(int dstId) {
+        DirectedSender<T> ds = directedSenders.get(dstId - 1);
+        int sent = ds.send(sendingSocket);
+        if (sent > 0) {
+            // System.out.println("Sent " + sent + " packets to " + ds.dstId);
+        }
+
+        if (sent > BACKOFF_DECREASE_LOWERBOUND) {
+            ds.resendPause /= BACKOFF_BASE_DOWN;
+            ds.resendPause = Math.max(ds.resendPause, MIN_RESENDPAUSE);
+        } else if (sent < BACKOFF_INCREASE_UPPERBOUND) {
+            ds.resendPause *= BACKOFF_BASE_UP;
+            ds.resendPause = Math.min(ds.resendPause, MAX_RESENDPAUSE);
+        }
+
+        // System.out.println("Resend pause for " + dstId + " is " + ds.resendPause);
+
+        // schedule next run in resendPause milliseconds
+        executor.schedule(() -> sendRoutine(dstId), ds.resendPause, TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -126,48 +132,8 @@ public class PerfectLink<T> {
      * @param content message to be sent
      */
     public void send(T content, Host to) {
-        sendBufferLock.lock();
-
-        try {
-            int toId = to.getId();
-            List<T> buffer = sendBuffers.get(toId - 1);
-            buffer.add(content);
-            if (buffer.size() >= 8) {
-                sendBuffer(toId);
-            }
-        } finally {
-            sendBufferLock.unlock();
-        }
-    }
-
-    public void flushBuffers() {
-        sendBufferLock.lock();
-        try {
-            for (int i = 0; i < sendBuffers.size(); i++) {
-                if (!sendBuffers.get(i).isEmpty()) {
-                    sendBuffer(i + 1);
-                }
-            }
-        } finally {
-            sendBufferLock.unlock();
-        }
-    }
-
-    private void sendBuffer(int hostId) {
-        List<T> buffer = sendBuffers.get(hostId - 1);
-        sendSendable(new Sendable(new DataPacket<>(packetCount.incrementAndGet(), myId,
-                new ArrayList<>(buffer)), hosts.get(hostId - 1)));
-        buffer.clear();
-    }
-
-    /**
-     * Add sendable to the sender Queue and update all related data structures
-     * Lock <code>limitLock</code> first
-     *
-     * @param se Sendable to be scheduled
-     */
-    private void sendSendable(Sendable se) {
-        resendWaitingQueue.offer(se);
+        int hostId = to.getId();
+        directedSenders.get(hostId - 1).schedule(content);
     }
 
     /**
@@ -175,144 +141,13 @@ public class PerfectLink<T> {
      * packets waiting to be sent are not sent
      */
     public void interruptAll() {
-        for (var senderThread : senderThreads) {
-            senderThread.interrupt();
-        }
         receiverThread.interrupt();
-        resendTimer.interrupt();
-    }
-
-    @SuppressWarnings({ "BusyWait", "StatementWithEmptyBody" })
-    private void resenderRoutine() {
-        Sendable s;
-        double[] history = new double[HISTORY_SIZE];
-        // init history to 1s
-        Arrays.fill(history, 1);
-        int historyIndex = 0;
-        while (true) {
-            try {
-                Thread.sleep(currentResendPause);
-            } catch (InterruptedException e) {
-                System.out.println("Resender routine stopped.");
-            }
-
-            flushBuffers();
-
-            int sentCount = 0;
-            double average = 0;
-            while ((s = resendWaitingQueue.poll()) != null && sentCount < MAX_SEND_AT_ONCE) {
-
-                if (confirmed.contains(s.n)) {
-                    confirmed.remove(s.n); // No need to keep track of this package anymore
-                } else {
-                    // remove all canceled messages from the packet
-                    List<T> messages = ((DataPacket<T>) s.message).data;
-                    try {
-                        final int toId = s.to.getId();
-                        messages.removeIf((m) -> isCanceled.apply(m, toId));
-                        if (messages.isEmpty()) {
-                            continue;
-                        }
-                    } catch (ConcurrentModificationException ignored) {
-                        // ignore, this is not a problem
-                    }
-
-                    sentCount++;
-                    // gather statistics for incremental backoff
-                    average += s.tryCount;
-
-                    senderQueue.offer(s);
-                }
-            }
-            average /= sentCount;
-
-            history[historyIndex] = average > 1 ? average : 1;
-            historyIndex = (historyIndex + 1) % HISTORY_SIZE;
-
-            // avgSentCount = sum of sentCount / HISTORY_SIZE, done with for loop
-            double avgSentCount = 0;
-            for (int i = 0; i < HISTORY_SIZE; i++) {
-                avgSentCount += history[i];
-            }
-            avgSentCount /= HISTORY_SIZE;
-            if (sentCount < MAX_SEND_AT_ONCE) {
-                continue;
-            }
-            if (avgSentCount > BACKOFF_INCREASE_LOWERBOUND) {
-                currentResendPause = Math.min(MAX_RESENDPAUSE, (int) (currentResendPause * BACKOFF_BASE_UP));
-                // System.out.println("[" + myId + "] Incrementing backoff delay to " +
-                // currentResendPause + ", sent " + this.packetCount);
-            } else if (avgSentCount < BACKOFF_DECREASE_UPPERBOUND) {
-                currentResendPause = Math.max(MIN_RESENDPAUSE, (int) (currentResendPause / BACKOFF_BASE_DOWN));
-                if (currentResendPause > MIN_RESENDPAUSE) {
-                    // System.out.println("[" + myId + "] Decrementing backoff delay to " +
-                    // currentResendPause + ", sent " + this.packetCount);
-                }
-            }
-        }
-    }
-
-    private class Sender extends Thread {
-        private final int ti;
-
-        public Sender(int ti) {
-            super();
-            this.ti = ti;
-        }
-
-        @Override
-        public void run() {
-            try {
-                senderRoutine();
-            } catch (InterruptedException e) {
-                System.out.println("Sender thread " + ti + " stopped.");
-            } catch (IOException e) {
-                System.err.println("Sender thread crashed for IOException");
-                e.printStackTrace();
-            }
-        }
-
-        @SuppressWarnings({ "InfiniteLoopStatement", "unchecked", "rawtypes", "BusyWait" })
-        private void senderRoutine() throws InterruptedException, IOException {
-            try (DatagramSocket s = new DatagramSocket()) {
-                while (true) {
-                    Sendable se = senderQueue.take();
-
-                    byte[] buf;
-                    boolean isAck = true;
-                    // prepare network packet
-                    if (se.message instanceof DataPacket) {
-                        buf = serializer.serialize((DataPacket) se.message);
-                        isAck = false;
-                    } else {
-                        buf = serializer.serialize((AckPacket) se.message);
-                    }
-                    DatagramPacket p = new DatagramPacket(buf, buf.length, InetAddress.getByName(se.to.getIp()),
-                            se.to.getPort());
-
-                    try {
-                        // send
-                        s.send(p);
-                    } catch (IOException e) {
-                        System.err.println(e.getMessage());
-                        System.out.println("Now sleeping");
-                        Thread.sleep(100);
-                        senderQueue.putFirst(se);
-                    }
-
-                    if (!isAck) {
-                        // increment tryCount and put on resendQueue
-                        se.tryCount++;
-                        resendWaitingQueue.offer(se); // Possible problem: if this thread crashes, se will be lost
-                    }
-                }
-            }
-        }
+        executor.shutdownNow();
     }
 
     class Receiver extends Thread {
         public Receiver() {
-            super();
+            super("Receiver");
         }
 
         @Override
@@ -337,9 +172,10 @@ public class PerfectLink<T> {
 
                     if (!serializer.isDatapacket(buff)) {
                         AckPacket ap = serializer.deserializeAckPacket(buff);
-                        confirmed.add(ap.n);
 
-                        // System.out.println("Received ack for packet " + ap.n);
+                        directedSenders.get(ap.receiver_id - 1).handleAck(ap);
+
+                        // System.out.println("Received ack for packet " + ap.n + " from " + ap.receiver_id);
                     } else {
                         DataPacket<T> dp = serializer.deserializeDataPacket(buff);
 
@@ -360,12 +196,22 @@ public class PerfectLink<T> {
         }
 
         private void sendAckForPacket(DataPacket<T> dp) {
-            final Host dst = hosts.get(dp.from - 1); // TODO: maybe too great of an assumption?
+            final Host dst = hosts.get(dp.from - 1);
 
-            Sendable se = new Sendable(new AckPacket(dp.n, myId), dst);
-            senderQueue.addFirst(se);
+            final AckPacket ap = new AckPacket(dp.n, myId);
+            // submit sending of ack to executor
+            executor.submit(() -> {
+                try {
+                    byte[] buff = serializer.serialize(ap);
+                    DatagramPacket p = new DatagramPacket(buff, buff.length, InetAddress.getByName(dst.getIp()),
+                            dst.getPort());
+                    sendingSocket.send(p);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
-            // System.out.println("sending ack to " + se.to.getId());
+            // System.out.println("sending ack to " + dp.from + " for packet " + dp.n);
         }
     }
 }
